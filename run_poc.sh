ea2dc74787688d1c9666b8926c1f560622daff7f
EC2 Default User <ec2-user@ip-172-31-31-215.us-west-1.compute.internal>
2023-02-03 22:59:28 +0000

    updated resume and shell scripts

--- run_poc.sh
+++ run_poc.sh
@@ -1,3 +1,14 @@
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Analytics" "remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Data Science" "remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Machine Learning" "remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice Presient Analytics" "remote" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Machine Learning Lead" "Remote" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head of Machine Learning" "Remote" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Data Science" "Remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Machine Learning" "Remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Analytics" "Remote" 250000 .15
+
+
 
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Data Science" "Los Angeles" 250000 .15 
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Machine Learning" "Los Angeles" 250000 .15 
@@ -5,10 +16,9 @@ cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Analytics" "
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Data Science" "Los Angeles" 250000 .15 
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Machine Learning" "Los Angeles" 250000 .15 
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice Presient Analytics" "Los Angeles" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Machine Learning Lead" "Los Angeles" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head of Machine Learning" "Los Angeles" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Data Science" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Machine Learning" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Analytics" "Los Angeles" 250000 .15
 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Data Science" "remote" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Machine Learning" "remote" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Analytics" "remote" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Data Science" "remote" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Machine Learning" "remote" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice Presient Analytics" "remote" 250000 .15
