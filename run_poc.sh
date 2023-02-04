14ea2202929ace26f817faf1f49072e898d69729
EC2 Default User <ec2-user@ip-172-31-31-215.us-west-1.compute.internal>
2023-02-04 02:14:58 +0000

    based on data analysis removed two title: manager and vice president from the searches

--- run_poc.sh
+++ run_poc.sh
@@ -4,11 +4,12 @@ cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Machin
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice Presient Analytics" "remote" 250000 .15
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Machine Learning Lead" "Remote" 250000 .15
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head of Machine Learning" "Remote" 250000 .15
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Data Science" "Remote" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Machine Learning" "Remote" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Analytics" "Remote" 250000 .15
-
-
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Data Science" "Remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead  Machine Learning" "Remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Analytics" "Remote" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Data Science" "Remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Machine Learning" "Remote" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Analytics" "Remote" 250000 .15
 
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Data Science" "Los Angeles" 250000 .15 
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Director Machine Learning" "Los Angeles" 250000 .15 
@@ -18,7 +19,9 @@ cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice President Machin
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Vice Presient Analytics" "Los Angeles" 250000 .15
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Machine Learning Lead" "Los Angeles" 250000 .15
 cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head of Machine Learning" "Los Angeles" 250000 .15
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Data Science" "Los Angeles" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Machine Learning" "Los Angeles" 250000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Manager Analytics" "Los Angeles" 250000 .15
-
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Data Science" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Machine Learning" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Analytics" "Los Angeles" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Data Science" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Machine Learning" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Analytics" "Los Angeles" 250000 .15
