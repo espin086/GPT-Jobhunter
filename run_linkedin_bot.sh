65b2cfa0eb50cc26b807beb04f65750311e9fd38
EC2 Default User <ec2-user@ip-172-31-31-215.us-west-1.compute.internal>
2023-02-05 22:27:24 +0000

    moved all helper functions into a helper folder

---
+++ run_linkedin_bot.sh
@@ -0,0 +1,27 @@
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Analytics" "remote" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Data Science" "remote" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Machine Learning" "remote" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice Presient Analytics" "remote" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Machine Learning Lead" "Remote" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head of Machine Learning" "Remote" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Lead Data Science" "Remote" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Lead  Machine Learning" "Remote" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Lead Analytics" "Remote" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 linkedn-bot.py "Head Data Science" "Remote" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head Machine Learning" "Remote" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head Analytics" "Remote" 250000 .2
+
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Data Science" "Los Angeles" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Machine Learning" "Los Angeles" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Analytics" "Los Angeles" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Data Science" "Los Angeles" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Machine Learning" "Los Angeles" 250000 .2 
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice Presient Analytics" "Los Angeles" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Machine Learning Lead" "Los Angeles" 250000 .12
+cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head of Machine Learning" "Los Angeles" 250000 .2
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Data Science" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Machine Learning" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Analytics" "Los Angeles" 250000 .15
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Data Science" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Machine Learning" "Los Angeles" 250000 .15 
+cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Analytics" "Los Angeles" 250000 .15
