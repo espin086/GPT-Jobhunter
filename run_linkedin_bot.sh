ef0365a2e80dfbb66136841cb58abe866cfd9624
EC2 Default User <ec2-user@ip-172-31-31-215.us-west-1.compute.internal>
2023-02-07 00:05:15 +0000

    refactored the shell script

--- run_linkedin_bot.sh
+++ run_linkedin_bot.sh
@@ -1,27 +1,25 @@
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Analytics" "remote" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Data Science" "remote" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Machine Learning" "remote" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice Presient Analytics" "remote" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Machine Learning Lead" "Remote" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head of Machine Learning" "Remote" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Lead Data Science" "Remote" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Lead  Machine Learning" "Remote" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Lead Analytics" "Remote" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 linkedn-bot.py "Head Data Science" "Remote" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head Machine Learning" "Remote" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head Analytics" "Remote" 200000 .2
+MY_PATH="/home/ec2-user/jobhunter"
+MIN_SIMILARITY=.2
+MIN_SALARY=200000
 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Data Science" "Los Angeles" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Machine Learning" "Los Angeles" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Director Analytics" "Los Angeles" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Data Science" "Los Angeles" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice President Machine Learning" "Los Angeles" 200000 .2 
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Vice Presient Analytics" "Los Angeles" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Machine Learning Lead" "Los Angeles" 200000 .12
-cd /home/ec2-user/linkedin-bot && python3 linkedin-bot.py "Head of Machine Learning" "Los Angeles" 200000 .2
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Data Science" "Los Angeles" 200000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Machine Learning" "Los Angeles" 200000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Head Analytics" "Los Angeles" 200000 .15
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Data Science" "Los Angeles" 200000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Machine Learning" "Los Angeles" 200000 .15 
-cd /home/ec2-user/linkedin-bot && python3 poc_linkedin.py "Lead Analytics" "Los Angeles" 200000 .15
+
+cd $MY_PATH && python3 linkedin-bot.py "Director Analytics" "remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Vice President Data Science" "remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Vice President Machine Learning" "remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Vice Presient Analytics" "remote" $MIN_SALARY $MIN_SIMILARITY
+
+cd $MY_PATH && python3 linkedin-bot.py "Machine Learning Lead" "Remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Head of Machine Learning" "Remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Lead Data Science" "Remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Lead  Machine Learning" "Remote" $MIN_SALARY $MIN_SIMILARITY 
+cd $MY_PATH && python3 linkedin-bot.py "Lead Analytics" "Remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedn-bot.py "Head Data Science" "Remote" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Head Machine Learning" "Remote" $MIN_SALARY $MIN_SIMILARITY 
+cd $MY_PATH && python3 linkedin-bot.py "Head Analytics" "Remote" $MIN_SALARY $MIN_SIMILARITY
+
+cd $MY_PATH && python3 linkedin-bot.py "Director Data Science" "Los Angeles" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Director Machine Learning" "Los Angeles" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Director Analytics" "Los Angeles" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Vice President Data Science" "Los Angeles" $MIN_SALARY $MIN_SIMILARITY
+cd $MY_PATH && python3 linkedin-bot.py "Vice President Machine Learning" "Los Angeles" $MIN_SALARY $MIN_SIMILARITY 
+
