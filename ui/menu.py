1e9f1cde2a3e21b0a135782a6bb9b33b7bb5cfab
JJ Espinoza <jjespinoza@JJs-MacBook-Pro.local>
2023-03-08 20:40:56 -0800

    ui: added getting latest job from prompt

--- ui/menu.py
+++ ui/menu.py
@@ -25,14 +25,10 @@ def menu():
                 os.system(f"python3 ../jobhunter/utils/job_title_generator.py --resume_file /Users/jjespinoza/Documents/jobhunter/jobhunter/resumes/resume.txt ")
             else:
                 print("Invalid file path. Please try again.")
-
-
-
-
-
             pass
         elif choice == '3':
             # Do view saved jobs
+            os.system(f"python3 ../jobhunter/utils/get_latest_jobs.py")
             pass
         elif choice == 'q':
             print("Exiting program...")
