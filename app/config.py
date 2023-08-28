bed0e8d04f0f90358918e6f039a39bb71ec3df0b
JJ Espinoza <jjespinoza@JJs-MacBook-Pro.local>
2023-08-27 19:52:05 -0600

    added ETL script and parametrized as much as possible

--- app/config.py
+++ app/config.py
@@ -42,30 +42,3 @@ WINDOW = 2
 MIN_COUNT = 1
 WORKERS = 4
 EPOCHS = 100
-
-import logging
-
-
-# === General Configs ===
-LOGGING_LEVEL = logging.INFO
-PROJECT_PATH = "dfffgfg/ffdddfgh/"
-
-
-# === Database Configs ===
-DATABASE_LOCATION = "ddssdd/yhgfggg/database.db"
-TABLE_JOBS = "jobs"
-TABLE_RESUMES = "resumes"
-TABLE_APPLICATIONS = "applications"
-
-
-# === API Configs ===
-JOB_SEARCH_URL = "https://linkedin-jobs-search.p.rapidapi.com/"
-JOB_SEARCH_X_RAPIDAPI_HOST = "linkedin-jobs-search.p.rapidapi.com"
-
-
-# === Model Configs ===
-VECTOR_SIZE = 50
-WINDOW = 2
-MIN_COUNT = 1
-WORKERS = 4
-EPOCHS = 100
