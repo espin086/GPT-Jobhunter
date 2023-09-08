95b9ddbe2797498f4c1e9e8471e0da441fda90cc
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2023-09-07 18:09:32 -0700

    added full tests to extract_salary.py function

--- jobhunter/src/extract_salary.py
+++ jobhunter/src/extract_salary.py
@@ -1,23 +1,27 @@
+import argparse
+import logging
 import re
 
 
 def extract_salary(text):
-    # Match for salaries like "$150,000.00" or "$150,000"
-    salary_pattern_1 = r"\$([\d,]+)(?:\.(\d{2}))?"
-    # Match for salaries like "$150K"
-    salary_pattern_2 = r"\$([\d\.]+)(K)"
-    # Match for salaries like "$125K-$150K" or "$150,000 - $350K"
-    salary_pattern_3 = (
-        r"\$([\d,]+)(?:\.(\d{2}))?\s*(K)?\s*-\s*\$([\d,]+)(?:\.(\d{2}))?(K)?"
-    )
+    # Setup logging
+    logging.basicConfig(level=logging.INFO)
 
-    # Find a match in the text for each pattern
+    # Regular expression patterns
+    salary_pattern_1 = r"\$([\d,]+)(?:\.(\d{2}))?"
+    salary_pattern_2 = r"\$([\d\.]+)(K)"
+    salary_pattern_3 = r"\$(?!401K)([\d,]+)(?:\.(\d{2}))?\s*(K)?\s*-\s*\$(?!401K)([\d,]+)(?:\.(\d{2}))?(K)?"
+    hourly_pattern = r"\$([\d\.]+)\s*to\s*\$([\d\.]+)\/hour"
+
+    # Search for patterns
     match1 = re.search(salary_pattern_1, text)
     match2 = re.search(salary_pattern_2, text)
     match3 = re.search(salary_pattern_3, text)
+    match4 = re.search(hourly_pattern, text)
+
+    salary_low, salary_high = None, None
 
     if match3:
-        # For salary ranges like "$125K-$150K" or "$150,000 - $350K"
         salary_low = (
             float(match3.group(1).replace(",", "")) * 1000
             if match3.group(3) == "K"
@@ -28,17 +32,41 @@ def extract_salary(text):
             if match3.group(6) == "K"
             else float(match3.group(4).replace(",", ""))
         )
+
     elif match2:
-        # For salaries like "$150K"
         salary_low = salary_high = float(match2.group(1).replace(",", "")) * 1000
+
     elif match1:
-        # For salaries like "$150,000.00" or "$150,000"
-        salary_low = salary_high = float(
-            match1.group(1).replace(",", "") + "." + match1.group(2)
+        salary_low = salary_high = (
+            float(match1.group(1).replace(",", "") + "." + match1.group(2))
             if match1.group(2)
-            else match1.group(1).replace(",", "")
+            else float(match1.group(1).replace(",", ""))
         )
-    else:
-        salary_low = salary_high = None
+
+    elif match4:
+        salary_low = float(match4.group(2)) * 40 * 52
+        salary_high = float(match4.group(3)) * 40 * 52
+
+    if salary_low is not None and salary_low < 100:
+        salary_low *= 1000
+    if salary_high is not None and salary_high < 100:
+        salary_high *= 1000
+
+    logging.info(f"Extracted salary range: {salary_low} to {salary_high}")
 
     return salary_low, salary_high
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Extract salary information from text."
+    )
+    parser.add_argument("text", type=str, help="Text containing salary information.")
+    args = parser.parse_args()
+
+    salary_low, salary_high = extract_salary(args.text)
+    print(f"Extracted salary range: {salary_low} to {salary_high}")
+
+
+if __name__ == "__main__":
+    main()
