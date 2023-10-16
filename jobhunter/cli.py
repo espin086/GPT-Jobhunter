fa6f6d0eeddb8e84055f5f6541a305e4ed7224fd
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2023-10-16 09:28:25 -0700

    hot fixt to the formatting of cli.py

--- jobhunter/cli.py
+++ jobhunter/cli.py
@@ -2,16 +2,18 @@ import click
 import os
 from streamlit.web import cli as stcli
 
+
 @click.command()
 def main():
     # Get the directory where cli.py is located
     cli_dir = os.path.dirname(os.path.abspath(__file__))
-    
+
     # Construct the path to main.py
     main_path = os.path.join(cli_dir, "main.py")
-    
+
     click.echo("Running the Streamlit app...")
     stcli.main(["run", main_path])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
