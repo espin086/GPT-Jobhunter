ea0a36a872200800266d9b5199440e8d10d8c3ee
Chris W <363708+0xchrisw@users.noreply.github.com>
2023-10-20 19:09:12 -0500

    Update test to use path from `config.py`

--- tests/resume_exists_test.py
+++ tests/resume_exists_test.py
@@ -2,6 +2,8 @@ import os
 import sys
 import pytest
 
+from jobhunter import config
+
 # Add the directory containing the extract_salary module to the Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
@@ -18,5 +20,5 @@ def test_resume_file_exists():
     Checks to see if the example resume exist, code should not run if this
     text does not exist
     """
-    file_path = "../temp/resumes/resume.txt"
+    file_path = config.RESUME_PATH
     assert os.path.isfile(file_path), f"File {file_path} not found"
